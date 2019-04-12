876208ca0788afc6a6294965df8fd0fad2f5e5fe
FRANCK Pierre-Antoine <pierre-antoine.franck@epitech.eu>
2019-04-12 11:06:25 +0200

    SiteVitrineV1 - Test for netlify

---
+++ mail.php
@@ -0,0 +1,22 @@
+<?php
+if ($_POST){
+    $firstname = $_POST['firstname'];
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+    $headers = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+    $headers .= "From: $name <$email>\r\nReply-to : $name <$email>\nX-Mailer:PHP";
+    $subject = "Contact Terradia";
+    $destinataire = "contact@terradia.eu";
+    $body="$message";
+    if(mail($destinataire,$subject,$body,$headers)){
+        $response['status'] = 'Succès';
+        $response['msg'] = 'Email envoyé avec succès';
+    } else {
+        $response['status'] = 'Erreur';
+        $response['msg'] = 'Une erreur est survenue';
+    }
+    echo json_encode($response);
+}
+?>
