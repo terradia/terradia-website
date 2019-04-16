4f95ee5368b27a2a5ccaaa253e357a8d68b66bbc
FRANCK Pierre-Antoine <pierre-antoine.franck@epitech.eu>
2019-04-16 12:05:04 +0200

    Delete headers

--- mail.php
+++ mail.php
@@ -4,19 +4,20 @@ if ($_POST){
     $email = $_POST['email'];
     $name = $_POST['name'];
     $message = $_POST['message'];
-    $headers = "MIME-Version: 1.0\r\n";
-    $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
-    $headers .= "From: $name <$email>\r\nReply-to : $name <$email>\nX-Mailer:PHP";
+    //$headers = "MIME-Version: 1.0\r\n";
+    //$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+    //$headers .= "From: $name <$email>\r\nReply-to : $name <$email>\nX-Mailer:PHP";
     $subject = "Contact Terradia";
     $destinataire = "contact@terradia.eu";
     $body="$message";
-    if(mail($destinataire,$subject,$body,$headers)){
+    mail($destinataire,$subject,$body);
+    /*if(mail($destinataire,$subject,$body)){
         $response['status'] = 'Succès';
         $response['msg'] = 'Email envoyé avec succès';
     } else {
         $response['status'] = 'Erreur';
         $response['msg'] = 'Une erreur est survenue';
-    }
-    echo $response;
+    }*/
+    //echo $response;
 }
 ?>
