6c4f0a5cc905fbcfadaa43970465975f07598287
FRANCK Pierre-Antoine <pierre-antoine.franck@epitech.eu>
2019-04-25 13:51:21 +0200

    Front-end modifications

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+    /*
+    PERMET DE CHANGER LA COULEUR DE LA NAVBAR ON SCROLL IL FAUDRA INSTANCIER LA CLASS .scroll dans le CSS.
+     */
+
+    /*window.onscroll = () => {
+        const header = document.querySelector('#header_nav');
+        if(this.scrollY <= 10)
+            header.className = '';
+        else
+            header.className = 'scroll';
+    };*/
+
+});
