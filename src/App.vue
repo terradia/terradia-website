bf3f6e437b68960b59e59e588e9b5f20f1ff9a3a
FRANCK Pierre-Antoine <pierre-antoine.franck@epitech.eu>
2020-01-15 15:47:44 -0500

    feat(vue): basic vuetify installation and adding assets + header

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <Header/>
+    <v-content>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  import Header from "./components/Header";
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="header">
+        <img src='../assets/images/logo-green.svg' class="logo">
+        <button class="btn">Je m'inscris dès maintenant</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+    .header {
+        padding: 1.5em;
+        flex-direction: column;
+    }
+
+    .logo {
+        height: 54px;
+        width: 270px;
+    }
+
+    .btn {
+        padding: 0.5em 3em;
+        border: 1px solid #5CC04A;
+        border-radius: 8px;
+        float: right;
+        color: #5CC04A;
+        margin-right: 2em;
+        margin-top: 0.7em;
+    }
+</style>
