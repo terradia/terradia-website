ded7b9961a5d7d8f6bce18ff249ac8030e02cd73
FRANCK Pierre-Antoine <pierre-antoine.franck@epitech.eu>
2020-01-24 21:42:50 -0500

    feat(particles): adding particles lib

---
+++ src/components/Particles.vue
@@ -0,0 +1,132 @@
+<template>
+    <div class="particles">
+        <div id="particles-js"></div>
+    </div>
+</template>
+
+<script>
+    import 'particles.js'
+    export default {
+        name: "particles",
+        mounted() {
+            this.initParticles()
+        },
+        methods: {
+            initParticles() {
+                window.particlesJS("particles-js", {
+                    "particles": {
+                        "number": {
+                            "value": 100,
+                            "density": {
+                                "enable": true,
+                                "value_area": 700
+                            }
+                        },
+                        "color": {
+                            "value": "#fff"
+                        },
+                        "shape": {
+                            "type": "triangle",
+                            "stroke": {
+                                "width": 0,
+                                "color": "#000000"
+                            },
+                            "polygon": {
+                                "nb_sides": 5
+                            },
+                        },
+                        "opacity": {
+                            "value": 1,
+                            "random": false,
+                            "anim": {
+                                "enable": false,
+                                "speed": 1,
+                                "opacity_min": 0.1,
+                                "sync": false
+                            }
+                        },
+                        "size": {
+                            "value": 6,
+                            "random": false,
+                            "anim": {
+                                "enable": true,
+                                "speed": 20,
+                                "size_min": 0.1,
+                                "sync": false
+                            }
+                        },
+                        "line_linked": {
+                            "enable": false,
+                            "distance": 150,
+                            "color": "#000000",
+                            "opacity": 0.4,
+                            "width": 1
+                        },
+                        "move": {
+                            "enable": true,
+                            "speed": 3,
+                            "direction": "top",
+                            "random": false,
+                            "straight": false,
+                            "out_mode": "out",
+                            "bounce": false,
+                            "attract": {
+                                "enable": false,
+                                "rotateX": 600,
+                                "rotateY": 1200
+                            }
+                        }
+                    },
+                    "interactivity": {
+                        "detect_on": "canvas",
+                        "events": {
+                            "onhover": {
+                                "enable": false,
+                                "mode": "grab"
+                            },
+                            "onclick": {
+                                "enable": false,
+                                "mode": "push"
+                            },
+                            "resize": false
+                        },
+                        "modes": {
+                            "grab": {
+                                "distance": 140,
+                                "line_linked": {
+                                    "opacity": 1
+                                }
+                            },
+                            "bubble": {
+                                "distance": 400,
+                                "size": 40,
+                                "duration": 2,
+                                "opacity": 8,
+                                "speed": 3
+                            },
+                            "repulse": {
+                                "distance": 200,
+                                "duration": 0.4
+                            },
+                            "push": {
+                                "particles_nb": 4
+                            },
+                            "remove": {
+                                "particles_nb": 2
+                            }
+                        }
+                    },
+                    "retina_detect": true
+                })
+            }
+        }
+    };
+</script>
+
+<style scoped>
+    #particles-js {
+        background: transparent;
+        height: 100%;
+        width: 100%;
+    }
+</style>
