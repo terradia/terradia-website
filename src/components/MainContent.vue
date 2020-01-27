dbd24f17135ccb5114d39adbe4b8e328289fa557
FRANCK Pierre-Antoine <pierre-antoine.franck@epitech.eu>
2020-01-26 20:39:59 -0500

    refactor(infos): some change on MainContent architecture

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -2,36 +2,16 @@
     <div>
         <Tractor/>
         <div class="infostext-wrapper">
-            <div class="text-section">
-        <InfosText
-                title="Découvrez"
-                text="Autour de vous, peut être dans la rue d’à côté, il y a un artisant qui fabrique la solution à vos problèmes. Terradia, vous aide à trouver les producteurs locaux autour d’on vous n’auriez jamais imaginé l’existance..."
-                data-aos="slide-up" data-aos-duration="2000"
-        />
-        <v-divider
-                class="mx-12 mt-2"
-        />
-        <InfosText
-                title="Commandez"
-                text="Grâce à Terradia, commandez vos produits locaux préférés, ou même des produits que vous ne connaissez pas encore a tout moment. Avec la livraison le jour même ou en J+1, profitez de vos achats instantanément..."
-                data-aos="slide-up" data-aos-duration="2000"
-        />
-        <v-divider
-                class="mx-12 mt-2"
-        />
-        <InfosText
-                title="Profitez"
-                text="Lorsque vous êtes livrés, l’expérience n’est pas terminée. Prenez votre temps pour profiter. Laissez un avis sur votre achat, découvrez les meilleurs conseils de producteurs, et découvrez les produits les plus en harmonie avec ce que vous avez choisi."
-                data-aos="slide-up" data-aos-duration="2000"
-        />
-            </div>
-            <img src="../assets/images/iphone-products-terradia.svg"
-                 class="iphone"
-                 data-aos="slide-left" data-aos-duration="2000"
-            >
+<!--            <div class="text-section">-->
+        <InfosText/>
+<!--            </div>-->
+<!--            <img src="../assets/images/iphone-products-terradia.svg"-->
+<!--                 class="iphone"-->
+<!--                 data-aos="slide-left" data-aos-duration="2000"-->
+<!--            >-->
         </div>
-        <Newsletter/>
         <Blog/>
+        <Newsletter/>
     </div>
 </template>
 
